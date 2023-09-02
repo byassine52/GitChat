1dd0cb9f09fce4978c136f0a7c7200c75bd93928
Yassine ElBadaoui <107576+byassine52@users.noreply.github.com>
2023-09-02 18:38:54 +0900

    Initial Commit

---
+++ GitChat/GitChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitChatApp.swift
+//  GitChat
+//
+//  Created by ياسين البدوي on 2023/09/02.
+//
+
+import SwiftUI
+
+@main
+struct GitChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
