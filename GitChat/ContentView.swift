b0a7dc80fe6a5dff1f862da9957e8dd88698ea63
Yassine ElBadaoui <107576+byassine52@users.noreply.github.com>
2023-09-02 19:53:48 +0900

    Convert indentations to tabs

--- GitChat/ContentView.swift
+++ GitChat/ContentView.swift
@@ -2,25 +2,25 @@
 //  ContentView.swift
 //  GitChat
 //
-//  Created by ياسين البدوي on 2023/09/02.
+//  Created by Yassine ElBadaoui on 2023/09/02.
 //
 
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+	var body: some View {
+		VStack {
+			Image(systemName: "globe")
+				.imageScale(.large)
+				.foregroundColor(.accentColor)
+			Text("Hello, world!")
+		}
+		.padding()
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var previews: some View {
+		ContentView()
+	}
 }
--- GitChat/GitChatApp.swift
+++ GitChat/GitChatApp.swift
@@ -2,16 +2,16 @@
 //  GitChatApp.swift
 //  GitChat
 //
-//  Created by ياسين البدوي on 2023/09/02.
+//  Created by Yassine ElBadaoui on 2023/09/02.
 //
 
 import SwiftUI
 
 @main
 struct GitChatApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
 }
